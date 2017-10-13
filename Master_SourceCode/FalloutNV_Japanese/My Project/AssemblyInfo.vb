628b65d6cd7d1adf5cb01ef81d210c00523acebf
NiG San <32508301+NiGSan@users.noreply.github.com>
2017-10-13 22:14:07 +0900

    Create AssemblyInfo.vb

---
+++ Master_SourceCode/FalloutNV_Japanese/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します。
+
+<Assembly: AssemblyTitle("FalloutNV Japanese Patch")> 
+<Assembly: AssemblyDescription("「Fallout New Vegas」の日本語化ツールです。面倒な作業もコレで終わり！")> 
+<Assembly: AssemblyCompany("NiG")> 
+<Assembly: AssemblyProduct("NiG")>
+<Assembly: AssemblyCopyright("Copyright © 2014-2017 NiG Application.")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
+<Assembly: Guid("e9b007a7-9b31-4a1e-8a02-50e13e59d34e")>
+
+' アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+' 既定値にすることができます:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("3.0.7.0")>
+<Assembly: AssemblyFileVersion("3.0.7.0")>
+
+<Assembly: NeutralResourcesLanguageAttribute("ja-JP")> 
